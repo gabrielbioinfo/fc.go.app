ee6d5d1b28d6b77f441bb2d2d9e86eb6da1245c1
Gabriel Espindola <gabrielbioinfo@gmail.com>
2021-07-23 05:50:54 -0300

    fix: :bug: fix call for sum function that is now private into the main package

--- app_test.go
+++ app_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestApp(t *testing.T) {
 
-	total := Sum(15, 15)
+	total := sum(15, 15)
 	if total != 30 {
 		t.Errorf("Invalid result! The result should be %d, but was %d", 30, total)
 	}
